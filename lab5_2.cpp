1d7d2928324137f00347b4b37e8ef87592dceca0
lab314-18@labeng.local <lab314-18@labeng.local>
2020-12-15 17:12:51 +0700

    Update5_2

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
-
+#include<cmath>
 using namespace std;
+
+double deg2rad(double a1){
+    double radian;
+    radian = a1*M_PI/180.0;
+    return radian;
+
+}
+
+double rad2deg(double a2){
+    double deg;
+    deg = a2*180.0/M_PI;
+    return deg;
+}
+double findXComponent(double l1 ,double l2,double a1,double a2){
+    double x1,x2;
+    x1 = l1*cos(a1);
+    x2 = l2*cos(a2);
+    return x1+x2;
+}
+double findYComponent(double l1,double l2,double a1,double a2){
+    double y1,y2;
+    y1 = l1*sin(a1);
+    y2 = l2*sin(a2);
+    return y1+y2;
+}
+
+double pythagoras(double x, double y){
+    double py = sqrt(pow(x,2)+pow(y,2));
+    return py;
+}
+
+void showResult(double k1, double k2){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = " << k1 << endl;
+    cout << "Direction of the resultant vector (deg) = " << k2 << endl;
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    
+
+
+
+}
+
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
